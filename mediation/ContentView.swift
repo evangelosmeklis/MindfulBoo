0800f5aa3043d9b4c93994fc0c5b7570f8c02fdf
Evangelos Meklis <vmeklis@hotmail.com>
2025-07-15 20:21:23 +0300

    Initial Commit

---
+++ mediation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mediation
+//
+//  Created by Evangelos Meklis on 15/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mediationTests/mediationTests.swift
@@ -0,0 +1,16 @@
+//
+//  mediationTests.swift
+//  mediationTests
+//
+//  Created by Evangelos Meklis on 15/7/25.
+//
+
+import Testing
+
+struct mediationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
